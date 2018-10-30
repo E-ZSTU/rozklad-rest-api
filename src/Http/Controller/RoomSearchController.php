cebe277427737d4783ffb0398249e542b7d4b641
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-10-30 05:04:45 +0200

    room searched route

---
+++ src/Http/Controller/RoomSearchController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Http\Controller;
+
+/**
+ * Class RoomSearchController
+ *
+ * @package App\Http\Controller
+ */
+class RoomSearchController
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
