8dcd44d9a73b957d7dde4ec958060867b565476b
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-12-23 16:33:06 +0200

    update dependency for request mapper.

---
+++ config/laravel-request-mapper.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    'exception-class' => \Maksi\LaravelRequestMapper\ValidationException\JsonResponsableException::class,
+];
--- config/request-mapper-l.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-return [
-    'exception-class' => \Maksi\LaravelRequestMapper\Exception\RequestMapperException::class,
-];
